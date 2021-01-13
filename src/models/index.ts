72e302a10831b820c5b5707bd69dcea23be50ce1
Extacy <git@extacy.cc>
2021-01-14 05:01:32 +1300

    Add "RuntimeErrors"
    Some client connection failures/errors shouldn't be retried.
    Such as no proxies being available
    
    This also just groups up all possible errors (excluding proxy errors) into one place

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,5 +1,5 @@
 export * from './accinfo';
-export * from './account-in-use-error';
+export * from "./runtime-errors";
 export * from './api-endpoints';
 export * from './autoloot';
 export * from './classes';
